89355fb04bbe24a0ea298be672f502567bdaea85
nikhil khare <nikhil.kharemy@gmail.com>
2018-05-28 12:27:47 +0530

    structure fixed

---
+++ api/channel.py
@@ -0,0 +1,25 @@
+class Channel:
+    channel_id = None
+    company_id = None
+    channel_name = None
+    logo = None
+    fb_handle = None
+    twt_handle = None
+    insta_handle = None
+    yt_handle = None
+    language = None
+    color_code = None
+    website = None
+    status = None
+    logged_in_user = None
+
+    def create(self, post):
+        self.company_id = post['company_id']
+        self.channel_name = post['channel_name']
+        self.company_id = post['company_id']
+        self.channel_name = post['channel_name']
+        self.company_id = post['company_id']
+        self.channel_name = post['channel_name']
+        self.company_id = post['company_id']
+        self.channel_name = post['channel_name']
+        self.company_id = post['company_id']
